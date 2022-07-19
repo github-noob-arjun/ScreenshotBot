8ce751c79da280c1952e8084817c411e8f533eee
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-19 21:57:53 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,18 +3,18 @@ from pathlib import Path
 
 class Config:
 
-    API_ID = int(os.environ.get('API_ID'))
-    API_HASH = os.environ.get('API_HASH')
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    API_ID = 6015447 #int(os.environ.get('API_ID'))
+    API_HASH = "0e96dd0dd4c4c9ded27c2ef58e6ab112" #os.environ.get('API_HASH')
+    BOT_TOKEN = "5459940804:AAFV-7L9uCA-D0006tEQvkVkpH9r6WKbp9U" #os.environ.get('BOT_TOKEN')
     SESSION_NAME = os.environ.get('SESSION_NAME', ':memory:')
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL'))
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split(' ')]
+    LOG_CHANNEL = -1644643944 #int(os.environ.get('LOG_CHANNEL'))
+    DATABASE_URL = "mongodb+srv://A:A@cluster0.i6jm9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" #os.environ.get('DATABASE_URL')
+    AUTH_USERS = [1266733572 1266733572] #[int(i) for i in os.environ.get('AUTH_USERS', '').split(' ')]
     MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 2))
     MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 600))
     TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
     SLOW_SPEED_DELAY = int(os.environ.get('SLOW_SPEED_DELAY', 5))
-    HOST = os.environ.get('HOST', '')
+    HOST = "https://mmngroups.herokuapp.com/"#os.environ.get('HOST', '')
     TIMEOUT = int(os.environ.get('TIMEOUT', 60 * 30))
     DEBUG = bool(os.environ.get('DEBUG'))
     IAM_HEADER = os.environ.get('IAM_HEADER', '')
