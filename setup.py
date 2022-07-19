e3e6bdd4a2966ae3ff927a03e6823a623b2a2464
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-19 22:40:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bot',
-      version='0.1.5',
+      version='0.1.6',
       description='Python module for interacting with Chatbase APIs',
       url='https://github.com/github-noob-arjun/ScreenshotBot',
       author='Github',
