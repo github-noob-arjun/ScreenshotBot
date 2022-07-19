ac1e230967447c194018661a4c9febdf3bf58c4c
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-19 17:44:30 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bot',
+      version='0.1.1',
+      description='Python module for interacting with Chatbase APIs',
+      url='https://github.com/github-noob-arjun/ScreenshotBot',
+      author='Github',
+      author_email='None',
+      license='MIT Pro',
+      packages=['bot'],      
+      zip_safe=False)
